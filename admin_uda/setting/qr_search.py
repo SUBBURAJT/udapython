38793e0617fba23a47b1dab5a5e0a216f7b40f16
pons <ponlakshmipons@gmail.com>
2021-12-31 19:13:47 +0530

    changes

--- admin_uda/setting/qr_search.py
+++ admin_uda/setting/qr_search.py
@@ -1,10 +1,9 @@
 from django.db.models.aggregates import Count
-from admin_uda.models import *
-import datetime as dt
+from admin_uda.models import Handon_form_workshop,Convention_form_workshop,Vendor_employees
 from hashids import Hashids
 
-class qr_search_list():
-    def list_registered_name(request):
+class qr_search_list:
+    def list_registered_name(self,request):
         if request.POST.get('keyword'):
             records=''
             status_res=0
@@ -16,43 +15,43 @@ class qr_search_list():
             if data:
                 status_res=1
                 group={}
-                for groupNames in data:
-                    group['userid']=groupNames.id
-                    group['id']=groupNames.work_id.id
-                    group['name']=groupNames.work_id.name
+                for group_names in data:
+                    group['userid']=group_names.id
+                    group['id']=group_names.work_id.id
+                    group['name']=group_names.work_id.name
                     group['usertype']='Workshop'
                     group['usertype_id']=1
                     resultval.append(group)
-                    records+="<tr><td style='padding: 10px;'>Workshop</td><td style='padding: 10px;'>"+groupNames.work_id.name+"</td></tr>"
+                    records+="<tr><td style='padding: 10px;'>Workshop</td><td style='padding: 10px;'>"+group_names.work_id.name+"</td></tr>"
             # FOR convention workshop
-            getAllUsers_con=Convention_form_workshop.objects.raw("SELECT A.*,B.id as bid,C.id as cid,C.name AS workshop_name FROM admin_uda_convention_form_workshop AS A JOIN admin_uda_handon_form AS B ON A.hand_id=B.id AND B.archive_id=0 AND B.status=1 LEFT JOIN admin_uda_convention_types AS C ON C.id=A.work_id AND C.status=1 WHERE A.name LIKE '%"+name+ "%' GROUP BY A.id",None)
-            if getAllUsers_con:
+            get_all_users_con=Convention_form_workshop.objects.raw("SELECT A.*,B.id as bid,C.id as cid,C.name AS workshop_name FROM admin_uda_convention_form_workshop AS A JOIN admin_uda_handon_form AS B ON A.hand_id=B.id AND B.archive_id=0 AND B.status=1 LEFT JOIN admin_uda_convention_types AS C ON C.id=A.work_id AND C.status=1 WHERE A.name LIKE '%"+name+ "%' GROUP BY A.id",None)
+            if get_all_users_con:
                 status_res=1
-                groupCon={}
-                for groupNames_con in getAllUsers_con:
-                    groupCon['userid']=groupNames_con.id
-                    groupCon['id']=groupNames_con.work_id
-                    groupCon['name']=groupNames_con.workshop_name
-                    groupCon['usertype']='Convention'
-                    groupCon['usertype_id']=2
-                    resultval.append(groupCon)
-                    records+="<tr><td style='padding: 10px;'>Convention</td><td style='padding: 10px;'>"+groupNames_con.workshop_name+"</td></tr>"
+                groupcon={}
+                for groupnames_con in get_all_users_con:
+                    groupcon['userid']=groupnames_con.id
+                    groupcon['id']=groupnames_con.work_id
+                    groupcon['name']=groupnames_con.workshop_name
+                    groupcon['usertype']='Convention'
+                    groupcon['usertype_id']=2
+                    resultval.append(groupcon)
+                    records+="<tr><td style='padding: 10px;'>Convention</td><td style='padding: 10px;'>"+groupnames_con.workshop_name+"</td></tr>"
             # For Exhibitor Staffs
-            getAllUsers_exh=Vendor_employees.objects.raw("SELECT ve.id, vrf.company_name, vrf.id AS vid FROM admin_uda_vendor_employees AS ve LEFT JOIN admin_uda_vendor_registration_form AS vrf ON vrf.id=ve.vendor_id WHERE ve.name LIKE '%"+name+ "%' GROUP BY ve.id",None)
-            if getAllUsers_exh:
+            get_all_users_exh=Vendor_employees.objects.raw("SELECT ve.id, vrf.company_name, vrf.id AS vid FROM admin_uda_vendor_employees AS ve LEFT JOIN admin_uda_vendor_registration_form AS vrf ON vrf.id=ve.vendor_id WHERE ve.name LIKE '%"+name+ "%' GROUP BY ve.id",None)
+            if get_all_users_exh:
                 status_res=1
-                groupExh={}
-                for groupNames_exh in getAllUsers_exh:
-                    groupExh['userid']=groupNames_exh.id
-                    groupExh['id']=groupNames_exh.vid
-                    groupExh['name']=groupNames_exh.company_name
-                    groupExh['usertype']='Exhibitor'
-                    groupExh['usertype_id']=3
-                    resultval.append(groupExh)
-                    records+="<tr><td style='padding: 10px;'>Exhibitor</td><td style='padding: 10px;'>"+groupNames_exh.company_name+"</td></tr>"
+                group_exh={}
+                for groupnames_exh in get_all_users_exh:
+                    group_exh['userid']=groupnames_exh.id
+                    group_exh['id']=groupnames_exh.vid
+                    group_exh['name']=groupnames_exh.company_name
+                    group_exh['usertype']='Exhibitor'
+                    group_exh['usertype_id']=3
+                    resultval.append(group_exh)
+                    records+="<tr><td style='padding: 10px;'>Exhibitor</td><td style='padding: 10px;'>"+groupnames_exh.company_name+"</td></tr>"
         return {"res":status_res,"records":records}
 
-    def list_registered_name_qr(request,ids,types):
+    def list_registered_name_qr(self,request,ids,types):
         status_res=0
         records=''
         if ids and types:
@@ -65,18 +64,18 @@ class qr_search_list():
                 con_id=cfw_hfw[1]
                 hashidcon = hashids.decode(con_id) 
                 dec_con_id=hashidcon[0]
-                getAllUsers_con=Convention_form_workshop.objects.raw("SELECT A.*,B.id as bid,C.id as cid,C.name AS workshop_name FROM admin_uda_convention_form_workshop AS A JOIN admin_uda_handon_form AS B ON A.hand_id=B.id AND B.archive_id=0 AND B.status=1 LEFT JOIN admin_uda_convention_types AS C ON C.id=A.work_id AND C.status=1 WHERE A.id='"+dec_con_id+"' AND A.hand_id='"+hidden_hand_id+"' GROUP BY A.id",None)
-                if getAllUsers_con:
+                get_all_users_con=Convention_form_workshop.objects.raw("SELECT A.*,B.id as bid,C.id as cid,C.name AS workshop_name FROM admin_uda_convention_form_workshop AS A JOIN admin_uda_handon_form AS B ON A.hand_id=B.id AND B.archive_id=0 AND B.status=1 LEFT JOIN admin_uda_convention_types AS C ON C.id=A.work_id AND C.status=1 WHERE A.id='"+dec_con_id+"' AND A.hand_id='"+hidden_hand_id+"' GROUP BY A.id",None)
+                if get_all_users_con:
                     status_res=1
-                    groupCon={}
-                    for groupNames_con in getAllUsers_con:
-                        groupCon['userid']=groupNames_con.id
-                        groupCon['id']=groupNames_con.work_id
-                        groupCon['name']=groupNames_con.workshop_name
-                        groupCon['usertype']='Convention'
-                        groupCon['usertype_id']=2
-                        resultval.append(groupCon)
-                        records+="<tr><td style='padding: 10px;'>Convention</td><td style='padding: 10px;'>"+groupNames_con.workshop_name+"</td></tr>"
+                    groupcon={}
+                    for groupnames_con in get_all_users_con:
+                        groupcon['userid']=groupnames_con.id
+                        groupcon['id']=groupnames_con.work_id
+                        groupcon['name']=groupnames_con.workshop_name
+                        groupcon['usertype']='Convention'
+                        groupcon['usertype_id']=2
+                        resultval.append(groupcon)
+                        records+="<tr><td style='padding: 10px;'>Convention</td><td style='padding: 10px;'>"+groupnames_con.workshop_name+"</td></tr>"
             if cfw_hfw[0]=='hfw':
                 work_id=cfw_hfw[1]
                 hashidwork = hashids.decode(work_id) 
@@ -85,13 +84,13 @@ class qr_search_list():
                 if data:
                     status_res=1
                     group={}
-                    for groupNames in data:
-                        group['userid']=groupNames.id
-                        group['id']=groupNames.work_id.id
-                        group['name']=groupNames.work_id.name
+                    for groupnames in data:
+                        group['userid']=groupnames.id
+                        group['id']=groupnames.work_id.id
+                        group['name']=groupnames.work_id.name
                         group['usertype']='Workshop'
                         group['usertype_id']=1
                         resultval.append(group)
-                        records+="<tr><td style='padding: 10px;'>Workshop</td><td style='padding: 10px;'>"+groupNames.work_id.name+"</td></tr>"
+                        records+="<tr><td style='padding: 10px;'>Workshop</td><td style='padding: 10px;'>"+groupnames.work_id.name+"</td></tr>"
         return {"res":status_res,"records":records}
 
