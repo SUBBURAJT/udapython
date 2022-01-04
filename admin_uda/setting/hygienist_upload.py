c0bcc30ad4f34dfe2d2d86bcf4b76c2a20f0d9c5
pons <ponlakshmipons@gmail.com>
2022-01-04 11:43:01 +0530

    hyginiest

--- admin_uda/setting/hygienist_upload.py
+++ admin_uda/setting/hygienist_upload.py
@@ -7,15 +7,12 @@ import random
 import csv
 
 class hygienist():
-    
     def add_hygienist(self,request):
         file=request
-        logoRoot = os.path.join(settings.MEDIA_ROOT, 'hygienist_upload/')
-        logoRoot=logoRoot.replace("\\","/")
+        logo_root = os.path.join(settings.MEDIA_ROOT, 'hygienist_upload/')
+        logo_root=logo_root.replace("\\","/")
         al_e_a=[]
-        ids=[]
         invalid_ada=[]
-        al_e=0
         add_count=0
         c_all=0
         err=''
@@ -24,9 +21,9 @@ class hygienist():
         t=str(random.randint(111111111, 9999999999))
         ext='.csv'
         new_file_name='hygienist_upload_'+d+t+ext
-        fs=FileSystemStorage(location=logoRoot)
-        filename=fs.save(new_file_name,file)
-        
+        fs=FileSystemStorage(location=logo_root)
+        fs.save(new_file_name,file)
+        err="File Not Found"
         if os.path.exists('uploads/hygienist_upload/'+new_file_name):
             with open('uploads/hygienist_upload/'+new_file_name, newline='') as csvfile:
                 datas = csv.reader(csvfile)
@@ -34,11 +31,10 @@ class hygienist():
                 de_header=['ID', 'First Name', 'Last Name', 'Address', 'City', 'State', 'Zip Code', 'Phone']
                 if len(headers) == 8 and headers==de_header:
                     result=self.save_update(datas)
+                    err=''
                     return {"exists_id":result['al_e_a'],"Added_id":result['add_count'],"err":err,'total_given':result['c_all'],'invalid_ada':result['invalid_ada']}
                 else:
                     err="Please Upload a Correct row and columns"
-        else:
-            err="File Not Found"
         return {"exists_id":al_e_a,"Added_id":add_count,"err":err,'total_given':c_all,'invalid_ada':invalid_ada}
 
     def save_update(self,datas):
@@ -46,19 +42,16 @@ class hygienist():
         c_all=0
         add_count=0
         al_e_a=[]
-        al_e=0
         invalid_ada=[]
-        ids=[]
         for fields in datas:
-            if fields:
-                if fields[0]!='ID':
+            if fields and fields[0]!='ID':
                     hy_id = rem_mul(fields[0])
                     first_name = rem_mul(fields[1])
                     last_name = rem_mul(fields[2])
                     address = rem_mul(fields[3])
                     city = rem_mul(fields[4])
                     state = rem_mul(fields[5])
-                    zip = rem_mul(fields[6])
+                    hzip = rem_mul(fields[6])
                     phone = rem_mul(fields[7])
                     c_all+=1
                     if hy_id.isnumeric():
@@ -70,11 +63,10 @@ class hygienist():
                             form.address = address
                             form.city = city
                             form.state = state
-                            form.zip = zip
+                            form.zip = hzip
                             form.phone = phone
                             form.save()
                             al_e_a.append(hy_id)
-                            al_e+=1
                         else:
                             form=Hygienist()
                             form.hygienist_id = hy_id
@@ -83,10 +75,9 @@ class hygienist():
                             form.address = address
                             form.city = city
                             form.state = state
-                            form.zip = zip
+                            form.zip = hzip
                             form.phone = phone
                             form.save()
-                            ids.append(form.id)
                             add_count+=1
                     else:
                         invalid_ada.append(hy_id)
