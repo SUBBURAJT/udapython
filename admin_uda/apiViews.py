4bfd8ade67bd4cb59b64427224d821d08bbdd905
pons <ponlakshmipons@gmail.com>
2022-01-04 14:14:03 +0530

    apiviews

--- admin_uda/apiViews.py
+++ admin_uda/apiViews.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.contrib import auth
 from django.contrib.auth.hashers import check_password
+from django.shortcuts import render
 from .models import Users
 from hashids import Hashids
 import random
@@ -8,6 +9,26 @@ import random
 authkey="123456"
 
 # Login
+def login_values_check(username,password):
+    res={}
+    res['status'] ='error'
+    res['msg'] ='Invalid Credentials'
+    if Users.objects.filter(status=1,email=username).exists():
+        data = Users.objects.values().get(status=1,email=username)
+        password_check=check_password(password,data['password'])
+        user = auth.authenticate(username=username, password=password,is_active=1)
+        if user is not None and (password_check==True):
+            hashids = Hashids(salt='UDAHEALTHDENTALSALT',min_length=20)
+            rand=random.randint(12345678909999999999,99999999991234567890)
+            hashid = hashids.encode(rand)
+            gen_rand=Users.objects.get(status=1,email=username)
+            gen_rand.api_random_key=hashid
+            gen_rand.save()
+            data_values = Users.objects.values('id','api_random_key','email','name').get(status=1,email=username)
+            res['status'] ='success'
+            res['msg'] ='Successfully login'
+            res['data'] =data_values
+    return res
 def login_check(request):
     global authkey
     res={}
@@ -20,23 +41,7 @@ def login_check(request):
             res['status'] ='error'
             res['msg'] ='Some field is empty'
             if(username != '' and password != ''):
-                res['status'] ='error'
-                res['msg'] ='Invalid Credentials'
-                if Users.objects.filter(status=1,email=username).exists():
-                    data = Users.objects.values().get(status=1,email=username)
-                    password_check=check_password(password,data['password'])
-                    user = auth.authenticate(username=username, password=password,is_active=1)
-                    if user is not None and (password_check==True):
-                        hashids = Hashids(salt='UDAHEALTHDENTALSALT',min_length=20)
-                        rand=random.randint(12345678909999999999,99999999991234567890)
-                        hashid = hashids.encode(rand)
-                        gen_rand=Users.objects.get(status=1,email=username)
-                        gen_rand.api_random_key=hashid
-                        gen_rand.save()
-                        data_values = Users.objects.values('id','api_random_key','email','name').get(status=1,email=username)
-                        res['status'] ='success'
-                        res['msg'] ='Successfully login'
-                        res['data'] =data_values
+                res=login_values_check(username,password)
         return JsonResponse(res,safe=False)
     else:
         res['status'] ='error'
